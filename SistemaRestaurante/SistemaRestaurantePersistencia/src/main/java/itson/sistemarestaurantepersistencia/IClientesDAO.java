e29a2793c728aa1811ac698f3df9f778e29eff66
SoyYuri09 <yuri.garcia252583@potros.itson.edu.mx>
2025-04-04 00:24:26 -0700

    Persistencia Clientes

--- SistemaRestaurante/SistemaRestaurantePersistencia/src/main/java/itson/sistemarestaurantepersistencia/IClientesDAO.java
+++ SistemaRestaurante/SistemaRestaurantePersistencia/src/main/java/itson/sistemarestaurantepersistencia/IClientesDAO.java
@@ -1,7 +1,13 @@
-
 package itson.sistemarestaurantepersistencia;
+
+import itson.sistemarestaurantedominio.Cliente;
+import itson.sistemarestaurantedominio.dtos.NuevoClienteDTO;
+import java.util.List;
 
 
 public interface IClientesDAO {
+    
+    public abstract Cliente registrar(NuevoClienteDTO nuevoClienteDTO);   
+    public abstract List<Cliente> consultar(String filtroBusqueda);
     
 }
